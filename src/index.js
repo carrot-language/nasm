fb53a809c5fcf9253a41e4e6bd8390bf040a36d1
William McGonagle <wpmcgonagle@gmail.com>
2022-02-06 22:17:18 -0500

    Created Src File

---
+++ src/index.js
@@ -0,0 +1,29 @@
+function getEmoji() {
+
+    return '💉';
+
+}
+
+function getName() {
+
+    return 'NASM';
+
+}
+
+function getStatus() {
+
+    // Needs Work
+    // In-Development
+    // Working
+    // Done
+    // Deprecated
+
+    return 0;
+
+}
+
+module.exports = {
+    getName,
+    getStatus,
+    getEmoji
+};
